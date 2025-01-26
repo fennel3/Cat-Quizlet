41c83fcf8e6daed1608e0ad7c546963a72515670
fennel3 <finley.champion@googlemail.com>
2025-01-26 21:01:13 +0000

    db cl

--- next-app/src/sharedCode/common.ts
+++ next-app/src/sharedCode/common.ts
@@ -1,9 +1,15 @@
 export function GetDBSettings() {
+   
     return {
       host: process.env.DATABASE_HOST,
       user: process.env.DATABASE_USER,
       password: process.env.DATABASE_PASSWORD,
       database: process.env.DATABASE_NAME,
       port: parseInt(process.env.DATABASE_PORT || "3306", 10), // Convert port to number
+      
+      
     };
+    
   }
+  
+  
