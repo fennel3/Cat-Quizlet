f7540e87e09dc9d2b6fad97f7f4629305ee5fe51
fennel3 <finley.champion@googlemail.com>
2025-01-28 18:07:49 +0000

    force dynamic on quiz page

--- next-app/src/app/quiz/page.tsx
+++ next-app/src/app/quiz/page.tsx
@@ -2,6 +2,8 @@
 import Question from "@/components/quiz/Question";
 import Quiz from "@/components/quiz/Quiz";
 
+export const dynamic = "force-dynamic";
+
 export default function Page() {
   return(
     <Quiz>
